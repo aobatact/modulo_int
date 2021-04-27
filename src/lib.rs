c727074c0d286f00cb9c8c01130f0b170f9a3949
aobatact <aobatact144@gmail.com>
2021-04-27 10:43:02 +0900

    rename bound to modulo, add Pow

--- src/lib.rs
+++ src/lib.rs
@@ -2,51 +2,60 @@ use const_generic_wrap::ConstWrap;
 use core::fmt::Debug;
 use num_traits::{
     ops::overflowing::{OverflowingAdd, OverflowingMul},
-    Bounded, Inv, Num, One, Zero,
+    Bounded, Inv, Num, One, Pow, ToPrimitive, Zero,
 };
 use std::{fmt::Display, ops::*};
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq, PartialOrd, Ord)]
-pub struct ModInt<T, B>
+pub struct ModInt<T, M>
 where
     T: Rem<T, Output = T>,
-    B: Into<T>,
+    M: Into<T>,
 {
     value: T,
-    bound: B,
+    modulo: M,
 }
 
-impl<T, B> ModInt<T, B>
+impl<T, M> ModInt<T, M>
 where
     T: Rem<Output = T> + Clone + PartialOrd + Zero,
-    B: Into<T> + Clone,
+    M: Into<T> + Clone,
 {
     pub fn new(value: T) -> Self
     where
-        B: ConstWrap<BaseType = T>,
+        M: ConstWrap<BaseType = T>,
     {
-        Self::new_with_bound(value, B::default())
+        Self::new_with_modulo(value, M::default())
     }
 
-    pub fn new_with_bound(value: T, bound: B) -> Self {
-        let t_bound = bound.clone().into();
-        debug_assert!(t_bound > T::zero());
-        let v = value.clone() % t_bound.clone();
+    pub fn new_with_modulo(value: T, modulo: M) -> Self {
+        let t_modulo = modulo.clone().into();
+        debug_assert!(t_modulo > T::zero());
+        let v = value.clone() % t_modulo.clone();
         Self {
-            value: if v < T::zero() { t_bound + v } else { v },
-            bound,
+            value: if v < T::zero() { t_modulo + v } else { v },
+            modulo,
         }
     }
+}
 
-    pub unsafe fn new_unchecked(value: T, bound: B) -> Self {
-        Self { value, bound }
+impl<T, M> ModInt<T, M>
+where
+    T: Rem<Output = T>,
+    M: Into<T>,
+{
+    pub unsafe fn new_unchecked(value: T, modulo: M) -> Self {
+        Self { value, modulo }
     }
 
-    pub fn bound(&self) -> &B {
-        &self.bound
+    pub fn modulo(&self) -> &M {
+        &self.modulo
     }
 
-    pub fn bound_val(&self) -> T {
-        self.bound.clone().into()
+    pub fn modulo_val(&self) -> T
+    where
+        M: Clone,
+    {
+        self.modulo.clone().into()
     }
 
     pub fn value(&self) -> &T {
@@ -54,13 +63,13 @@ where
     }
 }
 
-impl<T, B> ModInt<T, B>
+impl<T, M> ModInt<T, M>
 where
     T: PartialOrd<T> + Clone + Num + Bounded + Debug,
-    B: Into<T> + Clone,
+    M: Into<T> + Clone,
 {
     pub fn checked_inv(self) -> Option<Self> {
-        let b = self.bound_val();
+        let b = self.modulo_val();
 
         let (rs, rr) = if T::zero() == T::min_value() {
             //unsigned
@@ -124,109 +133,111 @@ where
             )
         };
 
-        if rr == T::one() {
+        if rr != T::one() {
             None
         } else {
-            Some(Self::new_with_bound(rs, self.bound))
+            Some(Self::new_with_modulo(rs, self.modulo))
         }
     }
 }
 
-impl<T, B> Display for ModInt<T, B>
+impl<T, M> Display for ModInt<T, M>
 where
     T: Rem<T, Output = T> + Display,
-    B: Into<T>,
+    M: Into<T>,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.value.fmt(f)
     }
 }
 
-impl<T, B> From<T> for ModInt<T, B>
+impl<T, M> From<T> for ModInt<T, M>
 where
     T: Rem<Output = T> + Clone + PartialOrd + Zero,
-    B: ConstWrap<BaseType = T> + Into<T> + Clone + Default,
+    M: ConstWrap<BaseType = T> + Into<T>,
 {
     fn from(value: T) -> Self {
         Self::new(value)
     }
 }
 
-impl<T, B> From<(T, B)> for ModInt<T, B>
+impl<T, M> From<(T, M)> for ModInt<T, M>
 where
     T: Rem<Output = T> + Clone + PartialOrd + Zero,
-    B: Into<T> + Clone + Default,
+    M: Into<T> + Clone,
 {
-    fn from(value: (T, B)) -> Self {
-        Self::new_with_bound(value.0, value.1)
+    fn from(value: (T, M)) -> Self {
+        Self::new_with_modulo(value.0, value.1)
     }
 }
 
-impl<T, B> Add<ModInt<T, B>> for ModInt<T, B>
+impl<T, M> Add<ModInt<T, M>> for ModInt<T, M>
 where
     T: Bounded + Clone + OverflowingAdd + PartialOrd + Rem<Output = T> + Zero,
-    B: Clone + Debug + Into<T> + PartialEq<B>,
+    M: Clone + Debug + Into<T> + PartialEq<M>,
 {
     type Output = Self;
-    fn add(self, rhs: ModInt<T, B>) -> Self::Output {
-        debug_assert_eq!(self.bound, rhs.bound);
+    fn add(self, rhs: ModInt<T, M>) -> Self::Output {
+        debug_assert_eq!(self.modulo, rhs.modulo);
         let (v, o) = self.value.overflowing_add(&rhs.value);
         let value = if o {
-            let b = self.bound_val();
+            let b = self.modulo_val();
             T::max_value() % b.clone() + v % b
         } else {
             v
         };
-        ModInt::new_with_bound(value, self.bound)
+        ModInt::new_with_modulo(value, self.modulo)
     }
 }
 
-impl<T, B> Sub<ModInt<T, B>> for ModInt<T, B>
+impl<T, M> Sub<ModInt<T, M>> for ModInt<T, M>
 where
     T: Rem<Output = T> + Clone + PartialOrd<T> + Zero + Bounded + OverflowingAdd + Sub<Output = T>,
-    B: Into<T> + PartialEq<B> + Clone + Debug,
+    M: Into<T> + PartialEq<M> + Clone + Debug,
 {
     type Output = Self;
-    fn sub(self, rhs: ModInt<T, B>) -> Self::Output {
-        debug_assert_eq!(self.bound, rhs.bound);
+    fn sub(self, rhs: ModInt<T, M>) -> Self::Output {
+        debug_assert_eq!(self.modulo, rhs.modulo);
         self + (-rhs)
     }
 }
 
-impl<T, B> Mul<ModInt<T, B>> for ModInt<T, B>
+impl<T, M> Mul<ModInt<T, M>> for ModInt<T, M>
 where
     T: Rem<Output = T> + Clone + PartialOrd<T> + Zero + Bounded + OverflowingMul,
-    B: Into<T> + PartialEq<B> + Clone + Debug,
+    M: Into<T> + PartialEq<M> + Clone + Debug,
 {
     type Output = Self;
-    fn mul(self, rhs: ModInt<T, B>) -> Self::Output {
-        debug_assert_eq!(self.bound, rhs.bound);
+    fn mul(self, rhs: ModInt<T, M>) -> Self::Output {
+        debug_assert_eq!(self.modulo, rhs.modulo);
         let (v, o) = self.value.overflowing_mul(&rhs.value);
         let value = if o {
-            let b = self.bound_val();
+            let b = self.modulo_val();
             T::max_value() % b.clone() + v % b
         } else {
             v
         };
-        ModInt::new_with_bound(value, self.bound)
+        ModInt::new_with_modulo(value, self.modulo)
     }
 }
 
-impl<T, B> Neg for ModInt<T, B>
+impl<T, M> Neg for ModInt<T, M>
 where
     T: PartialOrd<T> + Clone + Rem<Output = T> + Sub<Output = T> + Zero,
-    B: Into<T> + Clone,
+    M: Into<T> + Clone,
 {
     type Output = Self;
     fn neg(self) -> Self {
-        unsafe { Self::new_unchecked(self.bound_val() - self.value, self.bound) }
+        debug_assert!(self.value < self.modulo_val());
+        debug_assert!(self.value >= T::zero());
+        unsafe { Self::new_unchecked(self.modulo_val() - self.value, self.modulo) }
     }
 }
 
-impl<T, B> Zero for ModInt<T, B>
+impl<T, M> Zero for ModInt<T, M>
 where
     T: Bounded + Clone + OverflowingAdd + PartialOrd + Rem<Output = T> + Zero,
-    B: Clone + Debug + Into<T> + PartialEq<B> + ConstWrap<BaseType = T>,
+    M: Clone + Debug + Into<T> + PartialEq<M> + ConstWrap<BaseType = T>,
 {
     fn zero() -> Self {
         ModInt::new(T::zero())
@@ -237,7 +248,7 @@ where
     }
 }
 
-impl<T, B> One for ModInt<T, B>
+impl<T, M> One for ModInt<T, M>
 where
     T: Bounded
         + Clone
@@ -247,7 +258,7 @@ where
         + Rem<Output = T>
         + Zero
         + One,
-    B: Clone + Debug + Into<T> + PartialEq<B> + ConstWrap<BaseType = T>,
+    M: Clone + Debug + Into<T> + PartialEq<M> + ConstWrap<BaseType = T>,
 {
     fn one() -> Self {
         ModInt::new(T::one())
@@ -259,39 +270,39 @@ where
 }
 
 /// Calculate [Modular multiplicative inverse](https://en.wikipedia.org/wiki/Modular_multiplicative_inverse).
-impl<T, B> Inv for ModInt<T, B>
+impl<T, M> Inv for ModInt<T, M>
 where
     T: PartialOrd<T> + Clone + Num + Bounded + Debug,
-    B: Into<T> + Clone,
+    M: Into<T> + Clone,
 {
     type Output = Self;
     fn inv(self) -> Self {
         self.clone().checked_inv().expect(&format!(
-            "Cannot inverse. {:?} and {:?} is not coprimpe",
+            "Cannot inverse. {:?} and {:?} is not coprimpe.",
             self.value(),
-            self.bound_val()
+            self.modulo_val()
         ))
     }
 }
 
 /// ```
 /// # use modulo_int::*;
-/// use num_traits::*;
-/// use const_generic_wrap::*;
+/// use num_traits::Inv;
+/// use const_generic_wrap::WrapI32;
 /// let c17 = WrapI32::<17>;
 /// for i in 1..17 {
-///     let l = ModInt::new_with_bound(i, c17);
+///     let l = ModInt::new_with_modulo(i, c17);
 ///     for j in 1..17 {
-///         let r = ModInt::new_with_bound(j, c17);
+///         let r = ModInt::new_with_modulo(j, c17);
 ///         assert_eq!(l * r.inv(), l / r);
 ///         assert_eq!(l, (l / r) * r);
 ///     }
 /// }
 /// ```
-impl<T, B> Div for ModInt<T, B>
+impl<T, M> Div for ModInt<T, M>
 where
     T: PartialOrd<T> + Clone + Num + Bounded + Debug + Bounded + OverflowingMul,
-    B: Into<T> + PartialEq<B> + Clone + Debug,
+    M: Into<T> + PartialEq<M> + Clone + Debug,
 {
     type Output = Self;
 
@@ -300,22 +311,47 @@ where
     }
 }
 
-impl<T, B> Rem for ModInt<T, B>
+impl<T, M> Rem for ModInt<T, M>
 where
     T: PartialOrd<T> + Clone + Num,
-    B: Into<T> + Clone,
+    M: Into<T> + Clone,
 {
     type Output = Self;
 
     fn rem(self, rhs: Self) -> Self {
-        Self::new_with_bound(self.value % rhs.value, self.bound)
+        Self::new_with_modulo(self.value % rhs.value, self.modulo)
     }
 }
 
-impl<T, B> Num for ModInt<T, B>
+impl<T, M, E> Pow<E> for ModInt<T, M>
+where
+    T: PartialOrd<T> + Clone + Num,
+    M: Into<T> + Clone,
+    E: BitAnd<Output = E> + Clone + One + PartialOrd + ShrAssign<usize> + Zero,
+{
+    type Output = Self;
+
+    fn pow(self, mut rhs: E) -> Self {
+        let mut value = self.value().clone();
+        let modulo = self.modulo_val();
+        let mut result = T::one();
+        let ez = E::zero();
+        let eo = E::one();
+        while rhs > ez {
+            if (rhs.clone() & eo.clone()).is_one() {
+                result = (result * value.clone()) % modulo.clone();
+            }
+            rhs >>= 1;
+            value = (value.clone() * value) % modulo.clone();
+        }
+        Self::new_with_modulo(result, self.modulo)
+    }
+}
+
+impl<T, M> Num for ModInt<T, M>
 where
     T: Num + Bounded + Clone + Debug + PartialOrd<T> + OverflowingAdd + OverflowingMul,
-    B: Clone + Debug + ConstWrap<BaseType = T> + Into<T> + PartialEq,
+    M: Clone + Debug + ConstWrap<BaseType = T> + Into<T> + PartialEq,
 {
     type FromStrRadixErr = T::FromStrRadixErr;
 
@@ -324,17 +360,31 @@ where
     }
 }
 
-impl<T, B> Bounded for ModInt<T, B>
+impl<T, M> Bounded for ModInt<T, M>
 where
     T: Rem<T, Output = T> + Zero + Clone + PartialOrd,
-    B: Into<T> + Clone + ConstWrap<BaseType = T>,
+    M: Into<T> + Clone + ConstWrap<BaseType = T>,
 {
     fn min_value() -> Self {
         T::zero().into()
     }
 
     fn max_value() -> Self {
-        B::default().into().into()
+        M::default().into().into()
+    }
+}
+
+impl<T, M> ToPrimitive for ModInt<T, M>
+where
+    T: Rem<T, Output = T> + ToPrimitive,
+    M: Into<T>,
+{
+    fn to_i64(&self) -> Option<i64> {
+        self.value().to_i64()
+    }
+
+    fn to_u64(&self) -> Option<u64> {
+        self.value().to_u64()
     }
 }
 
@@ -346,7 +396,7 @@ mod tests {
     #[test]
     fn add() {
         let c17 = WrapU32::<17>;
-        let l = ModInt::new_with_bound(11, c17);
+        let l = ModInt::new_with_modulo(11, c17);
         assert_eq!(11, *l.value());
         let r = 35.into();
         let lr = l + r;
@@ -354,12 +404,15 @@ mod tests {
         let r = 36.into();
         let lr = l + r;
         assert_eq!(13, *lr.value());
+        let r = 40.into();
+        let lr = l + r;
+        assert_eq!(0, *lr.value());
     }
 
     #[test]
     fn sub() {
         let c17 = WrapU32::<17>;
-        let l = ModInt::new_with_bound(11, c17);
+        let l = ModInt::new_with_modulo(11, c17);
         assert_eq!(11, *l.value());
         let r = 35.into();
         let lr = l - r;
@@ -372,7 +425,7 @@ mod tests {
     #[test]
     fn mul() {
         let c17 = WrapU32::<17>;
-        let l = ModInt::new_with_bound(11, c17);
+        let l = ModInt::new_with_modulo(11, c17);
         assert_eq!(11, *l.value());
         let r = 35.into();
         let lr = l * r;
@@ -386,29 +439,35 @@ mod tests {
     fn inv_u() {
         let c17 = WrapU32::<17>;
         for i in 1..17 {
-            let l = ModInt::new_with_bound(i, c17);
+            let l = ModInt::new_with_modulo(i, c17);
             assert_eq!(i, *l.value());
             assert_eq!(1, *(l * l.inv()).value());
         }
+        let c12 = WrapU32::<12>;
+        assert!(ModInt::new_with_modulo(4, c12).checked_inv().is_none());
+        assert!(ModInt::new_with_modulo(8, c12).checked_inv().is_none());
     }
 
     #[test]
     fn inv_i() {
         let c17 = WrapI32::<17>;
         for i in 1..17 {
-            let l = ModInt::new_with_bound(i, c17);
+            let l = ModInt::new_with_modulo(i, c17);
             assert_eq!(i, *l.value());
             assert_eq!(1, *(l * l.inv()).value());
         }
+        let c12 = WrapI32::<12>;
+        assert!(ModInt::new_with_modulo(4, c12).checked_inv().is_none());
+        assert!(ModInt::new_with_modulo(8, c12).checked_inv().is_none());
     }
 
     #[test]
     fn div() {
         let c17 = WrapI32::<17>;
         for i in 1..17 {
-            let l = ModInt::new_with_bound(i, c17);
+            let l = ModInt::new_with_modulo(i, c17);
             for j in 1..17 {
-                let r = ModInt::new_with_bound(j, c17);
+                let r = ModInt::new_with_modulo(j, c17);
                 assert_eq!(l, (l / r) * r);
             }
         }
